5f7670b42fbc76db459fe3200d07495ae454ad5b
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-04-05 09:58:34 -0400

    Conditionally render app based on geolocation
    set up cases for when user presses allow, block, or hasnt pressed either yet

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return <div>Hi there!</div>;
-};
+class App extends React.Component {
+    // Javascript constructor method
+    constructor(props) {
+        super(props); // Always need super function first
+
+        // THIS IS THE ONLY TIME we do direct assignment to this.state!!!
+        this.state = { lat: null, errorMessage: "" };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // remember to always use setState
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => {
+                // setState is additive; it will not replace the above statement but add to it
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    // React says we have to define render method!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
