24dcf7224d0f337157b11680768da8a8e937dbad
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-04-08 15:23:37 -0400

    hook up unsplash
    for retrieving pics later

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,20 +3,27 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get("https://api.unsplash.com/search/photos", {
-            params: { query: term },
-            headers: {
-                Authorization:
-                    "Client-ID d107cf86e62fd8b70d2b14e21b01591497e34e748572dd4b74549ed0f6f2fa72"
+    state = { images: [] };
+
+    onSearchSubmit = async term => {
+        const response = await axios.get(
+            "https://api.unsplash.com/search/photos",
+            {
+                params: { query: term },
+                headers: {
+                    Authorization:
+                        "Client-ID d107cf86e62fd8b70d2b14e21b01591497e34e748572dd4b74549ed0f6f2fa72"
+                }
             }
-        });
-    }
+        );
+        this.setState({ images: response.data.results });
+    };
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: "10px" }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
