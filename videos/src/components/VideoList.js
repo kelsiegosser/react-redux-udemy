99602117682848ba20cd29f1a46738056c2968db
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-04-09 13:46:59 -0400

    Include video items for video list

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,6 +1,12 @@
 import React from "react";
-const VideoList = props => {
-    return <div>{props.videos.length}</div>;
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map(video => {
+        return <VideoItem video={video} />;
+    });
+
+    return <div>{renderedList}</div>;
 };
 
 export default VideoList;
