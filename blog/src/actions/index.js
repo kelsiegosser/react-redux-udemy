5dc6a4cb75222b889afc8ff623865412bf475eb5
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-04-16 10:19:23 -0400

    finding unique user ids

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,16 +1,29 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id))); //dont need await in front of dispatch because we don't need any other info after this is called
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => _fetchUser(id, dispatch);
-// private function when prepending with _
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// // private function when prepending with _
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
